57681f3b377a530cb638b605d49a78379c338493
Eric Larson <eclarson@smu.edu>
2017-11-09 10:52:11 -0600

    adding model export for tornado pymongo models

--- export_coreml.py
+++ export_coreml.py
@@ -10,35 +10,44 @@ from pymongo.errors import ServerSelectionTimeoutError
 
 # model imports
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+
+from sklearn.preprocessing import StandardScaler
+
 import numpy as np
 
 # export 
 import coremltools
 
 
-dsid = 3
+dsid = 2
 client  = MongoClient(serverSelectionTimeoutMS=50)
 db = client.sklearndatabase
 
 
-# create feature vectors from database
+# create feature/label vectors from database
 X=[];
-for a in db.labeledinstances.find({"dsid":dsid}): 
-    X.append([float(val) for val in a['feature']])
-
-# create label vector from database
 y=[];
 for a in db.labeledinstances.find({"dsid":dsid}): 
-    y.append(a['label'])
+    X.append([float(val) for val in a['feature']])
+    y.append(a['label'])    
 
 
 print("Found",len(y),"labels and",len(X),"feature vectors")
 print("Unique classes found:",np.unique(y))
 
-clf = RandomForestClassifier(n_estimators=50)
+clf = RandomForestClassifier(n_estimators=150)
+clf_svm = SVC()
+clf_pipe = Pipeline([("SCL", StandardScaler()),
+	("SVC",SVC())])
+
 print("Training Model", clf)
 
 clf.fit(X,y)
+clf_svm.fit(X,y)
+clf_pipe.fit(X,y)
 
 print("Exporting to CoreML")
 
@@ -48,4 +57,25 @@ coreml_model = coremltools.converters.sklearn.convert(
 # save out as a file
 coreml_model.save('RandomForestAccel.mlmodel')
 
+
+coreml_model = coremltools.converters.sklearn.convert(
+	clf_svm) 
+
+# save out as a file
+coreml_model.save('SVMAccel.mlmodel')
+
+coreml_model = coremltools.converters.sklearn.convert(
+	clf_pipe) 
+
+# save out as a file
+coreml_model.save('PipeAccel.mlmodel')
+ 
+
+# save out as a file
+
 client.close() 
+
+
+
+
+
